d37e4ba3ca8f4865c4853108f78e57f17fca7f43
LiorBaraban <barabanlior@gmail.com>
2019-11-27 14:26:24 +0200

    added hello world 00.js

---
+++ src/00.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    console.log('incoming request');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+})
+
+server.listen(3000, () => {
+    console.log('server is listening!');
+});
+
+
+
+
+
